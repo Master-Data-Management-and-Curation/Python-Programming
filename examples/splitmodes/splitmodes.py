30d0b38934def57d76c63632226b2059d353fefb
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2024-10-01 16:39:46 +0200

    implement channel splitter
    no sanitation of inputs
    close #1

--- examples/splitmodes/splitmodes.py
+++ examples/splitmodes/splitmodes.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 from sys import argv
+from sys import stderr
+from PIL import Image
 
 def read_images(input_dir):
     files = input_dir.glob("*.png")
@@ -13,19 +15,44 @@ if __name__ == "__main__":
     # If I call this program with
     # python splitmodes.py /my/images/
     # argv[1] will contain "/my/images"
-    input_dir = Path(argv[1])
+    try:
+        input_dir = Path(argv[1])
+    except IndexError:
+        print("Usage: <input dir>", file=stderr)
+        exit(1)
+
     img_list = read_images(input_dir)
-    print(img_list)
-    exit(0)
+    # TODO: Use logging library to debug-log the 
+    #       content of img_list
 
     # 3. Create the output folder at the same level as the 
-    out_dir = str(input_dir) + "_channels"
-
-
     #     input folder and name it as input folder + suffix "_channels"
+    out_dir = Path(str(input_dir.absolute()) + "_channels")
+    try:
+        out_dir.mkdir()
+    except FileExistsError:
+        print(f"Output dir {out_dir} already exists.")
+        exit(1)
+
+
+    # HINT: you need to install the `pillow` library and find how 
+    # to read images in the documentation
+
+    # im = Image.open("hopper.ppm")
+    # im.save("out.png")
 
     # 4. Loop over valid images
-    #     4.1 For each channel
-    #         4.1.1 Create the monochromatic image
-    #         4.1.2 save it in output dir by using the input file name + suffix (r,g,b)
+    chan_to_idx = {0: "R", 1: "G", 2: "B"}
+
+    for img_file in img_list:
+        im = Image.open(img_file)
+        channels = im.split()
+
+        for i, c in enumerate(channels):
+            try:
+                out_file = out_dir / f"{img_file.stem}_{chan_to_idx[i]}.png"
+            except KeyError as e:
+                print("discaring alpha channel", file=stderr)
+                break
+            c.save(out_file)
     exit(0)
